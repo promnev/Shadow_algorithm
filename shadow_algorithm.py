bc2b74ef9c31631de6970701496ab54184df4264
promnev <promnevkiev@gmail.com>
2023-05-26 01:31:36 +0200

    added duplicate check function for base numbery

--- shadow_algorithm.py
+++ shadow_algorithm.py
@@ -4,35 +4,52 @@ import time
 level = 0
 table_list = []
 
-print('The game generates a 2-, 3-, or 4-digit number depending on the selected difficulty level\nand a simple algorithm based on the digits of this number and one simple arithmetic operation\n(addition, or modulo difference), after which it returns 9 pairs of numbers (before and after transformation by the algorithm).\nYour task is to understand the algorithm and calculate the pair for the 10th number.')
+print(
+    'The game generates a 2-, 3-, or 4-digit number depending on the selected difficulty level\nand a simple algorithm based on the digits of this number and one simple arithmetic operation\n(addition, or modulo difference), after which it returns 9 pairs of numbers (before and after transformation by the algorithm).\nYour task is to understand the algorithm and calculate the pair for the 10th number.')
 
-while level == 0:
-    level_question = input('Please select the difficulty level by typing the appropriate command: flexi\perm\senior\n')
+level_question = input('Please select the difficulty level by typing the appropriate command: flexi\perm\senior\n')
 
-    if level_question == 'flexi':
-        level = 1
-    if level_question == 'perm':
-        level = 2
-    if level_question == 'senior':
-        level = 3
+if level_question == 'flexi':
+    level = 1
+if level_question == 'perm':
+    level = 2
+if level_question == 'senior':
+    level = 3
 
 base_len = level + 1
 random_scope = random.randint(2, base_len)
 random_base_indexes_list = random.choices([n + 1 for n in range(base_len)], k=random_scope)
 random_aggregator = random.choice(['+', '-'])
 
+
 def generate_base(level):
     if level == 1:
-        return random.randint(10, 99)
+        return random.randint(10, 100)
     if level == 2:
         return random.randint(100, 999)
     if level == 3:
         return random.randint(1000, 9999)
 
 
+def generate_base_list(level):
+    base_list = []
+
+    for n in range(11):
+        base = generate_base(level)
+        base_list.append(base)
+
+    base_set = set(base_list)
+
+    if len(base_list) == len(base_set):
+        return base_list
+    else:
+        return generate_base_list(level)
+
+
 def algorithm(base):
+    global result
     base_as_list = [n for n in str(base)]
-    increment_list = [base_as_list[(n-1)] for n in random_base_indexes_list]
+    increment_list = [base_as_list[(n - 1)] for n in random_base_indexes_list]
     increment_str = ''.join(increment_list)
     increment_int = int(increment_str)
 
@@ -44,7 +61,7 @@ def algorithm(base):
     return result
 
 
-def play(level):
+def play():
     enter_text = 'algorithm generation'
     loading_text = '.....\n'
 
@@ -56,9 +73,10 @@ def play(level):
         time.sleep(0.5)
         print(i, end='', flush=True)
 
-    for n in range(11):
-        base = generate_base(level)
-        table_list.append(f'{base} {algorithm(base)}')
+    base_list = generate_base_list(level)
+
+    for n in base_list:
+        table_list.append(f'{n} {algorithm(n)}')
 
     for n in table_list[:9]:
         print(n)
@@ -76,12 +94,9 @@ def play(level):
         if question == answer:
             print('Right!\n')
             game_over = True
-            end = input('Game over')
+        #if question == 'debug':
+            #print(answer)
         else:
             print('Wrong')
 
-play(level)
-
-
-
-
+play()
